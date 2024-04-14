84f7a3a1a0762f563f746b28e2a730a7d4ab04dc
Ayechanpaing31 <116068529+Ayechanpaing31@users.noreply.github.com>
2024-04-15 06:42:46 +0800

    first commit

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import { Loader2, LucideProps } from 'lucide-react'
+import { cn } from '../../lib/utils'
+
+interface SpinnerProps extends LucideProps {}
+
+const Spinner: FC<SpinnerProps> = ({ className, ...props }) => {
+  return (
+    <div className={cn('mr-2 h-4 w-4 animate-spin', className)}>
+      <Loader2 {...props} />
+    </div>
+  )
+}
+
+export default Spinner;
